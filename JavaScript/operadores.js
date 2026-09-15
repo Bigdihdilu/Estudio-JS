d0900263bdae063f0a237a83851426a9b669a18f
dilu <sevesant24@gmail.com>
2026-09-15 16:16:43 -0400

    SecCommit

---
+++ JavaScript/operadores.js
@@ -0,0 +1,4 @@
+//operadores
+var num1 = 5;
+    num1 = num1 + 8;
+console.log(num1);
--- JavaScript/string.js
+++ JavaScript/string.js
@@ -1,9 +1,23 @@
-let fruta = "banana";
-console.log(fruta.length);
+// let curso = 'javascript'
+// console.log(curso.indexOf('c')); dice numericamente en que posicion esta la letra
 
-const numero = [1, 2, 3, 4, 5]; //87rtehjkr typrof
-let num = 1;
-numero.map((num) => {
-    console.log(num);
-});
-console.log(typeof num);
+// let curso = 'javascript'
+// console.log(curso.replace('javascript' , 'php')); cambia el valor de la variable
+
+// let curso = 'javascript'
+// console.log(curso.split('-')); separa en letras el valor o lo convierte en un array
+
+//convertir
+var numero_uno = "2";
+var numero_dos = "3";
+console.log(Number(numero_uno) + Number(numero_dos))
+
+
+//const numero = [1, 2, 3, 4, 5]; //87rtehjkr typrof
+//let num = 1;
+//numero.map((num) => {
+//    if(num > 4){
+//        console.log(num);
+//    }
+//});
+//console.log(typeof num);
